a9f6a942f63afa656b844eeacf6e86ed72bff9d8
Mitali Harsh <mitali.harsh@sunarctechnologies.com>
2023-06-05 14:46:09 +0530

    musicly project with spotify login

--- src/Components/BrowseSlider/Categories.jsx
+++ src/Components/BrowseSlider/Categories.jsx
@@ -1,25 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import { getCategory } from '../../spotify';
+import { getCategory } from './spotify';
 import { Title } from '../Styled'
 import Slider from './Slider';
 
 function Categories() {
   const [ categories , setCategory] = useState([]);
 
-  useEffect(() =>{
-    const fetchCategory = async() =>{
-      try{
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
         const data = await getCategory();
         setCategory(data.data.categories.items);
-
-      }
-      catch(e){
+      } catch (e) {
         console.log(e);
       }
-    }
+    };
     fetchCategory();
-    
-  },[])
+  }, []);
   return (
     <>
       <Title padding="10px 0">Popular Category</Title>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import MusicPlayer from '../Components/MusicPlayer/MusicPlayer'
+// import MusicPlayer from '../Components/MusicPlayer/MusicPlayer'
 import Nav from '../Components/NavBar/Nav'
 import SideBar from '../Components/SideBar/SideBar'
 import { Container } from '../Components/Styled'
